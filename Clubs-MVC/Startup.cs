25b5e7f39ff16e2f1bb8a9c50b818be5be3ee932
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2022-01-04 13:48:48 +0000

    base setup

---
+++ Clubs-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clubs_MVC.Startup))]
+namespace Clubs_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
