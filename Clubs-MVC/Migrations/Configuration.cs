4da868527c83e1046d530e1f5cd16dce49b9cb16
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2022-01-04 14:14:57 +0000

    Second Migration and App User seeded

--- Clubs-MVC/Migrations/Configuration.cs
+++ Clubs-MVC/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 ﻿namespace Clubs_MVC.Migrations
 {
+    using Clubs_MVC.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +21,61 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            {
+                var manager =
+                    new UserManager<ApplicationUser>(
+                        new UserStore<ApplicationUser>(context));
+
+                var roleManager =
+                    new RoleManager<IdentityRole>(
+                        new RoleStore<IdentityRole>(context));
+
+                context.Roles.AddOrUpdate(r => r.Name,
+                    new IdentityRole { Name = "Admin" }
+                    );
+                context.Roles.AddOrUpdate(r => r.Name,
+                    new IdentityRole { Name = "ClubAdmin" }
+                    );
+                context.Roles.AddOrUpdate(r => r.Name,
+                    new IdentityRole { Name = "member" }
+                    );
+
+                PasswordHasher ps = new PasswordHasher();
+
+                context.Users.AddOrUpdate(u => u.UserName,
+                    new ApplicationUser
+                    {
+                        UserName = "powell.paul@itsligo.ie",
+                        Email = "powell.paul@itsligo.ie",
+                        EmailConfirmed = true,
+                        JoinDate = DateTime.Now,
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                        FirstName = "Paul",
+                        Surname = "Powell",
+                        PasswordHash = ps.HashPassword("Rad3022021$1")
+                    });
+
+                context.Users.AddOrUpdate(u => u.UserName,
+                    new ApplicationUser
+                    {
+                        UserName = "radp2016@outlook.com",
+                        Email = "radp2016@outlook.com",
+                        EmailConfirmed = true,
+                        JoinDate = DateTime.Now,
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                        FirstName = "Rad",
+                        Surname = "Paulner",
+                        PasswordHash = ps.HashPassword("radP2016$1")
+                    });
+                context.SaveChanges();
+
+                ApplicationUser admin = manager.FindByEmail("powell.paul@itsligo.ie");
+                if (admin != null)
+                {
+                    manager.AddToRoles(admin.Id, new string[] { "Admin", "member", "ClubAdmin" });
+                }
+            }
         }
     }
 }
+
