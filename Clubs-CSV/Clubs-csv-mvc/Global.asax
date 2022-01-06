126c44bb461447068169b2becf48cb2e7ee0a9e8
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2022-01-06 13:04:49 +0000

    week4 project setup

---
+++ Clubs-CSV/Clubs-csv-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clubs_csv_mvc.MvcApplication" Language="C#" %>
---
+++ Clubs-CSV/Clubs-csv-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clubs_csv_mvc.Startup))]
+namespace Clubs_csv_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
